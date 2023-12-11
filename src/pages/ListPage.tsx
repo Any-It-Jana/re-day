6f338de5ee8b14b0a4d6a35b9c941588df4b26f0
L-Hyun <dkh1001@outlook.kr>
2023-12-12 00:00:05 +0900

    x

--- src/pages/ListPage.tsx
+++ src/pages/ListPage.tsx
@@ -14,6 +14,8 @@ const ListPage = () => {
     getRecordList(token || "").then((res) => {
       if (res.statusCode === 200) {
         setLst(res.body.list);
+        // 이거 리턴이 정확하게 뭔 지 모르겠는데
+        // 리턴에 맞게 res.body.이거 채우면 됨
       }
     });
   }, []);
